fa7867c0169f058088cd377e3341ae9b6d7ef11f
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-09-23 18:33:23 +0800

    Update CONFIG.py

--- src/CONFIG.py
+++ src/CONFIG.py
@@ -15,7 +15,7 @@ if osuname == "prathmeshmadhu":
     experiments_path = "/localhome/prathmeshmadhu/work/EFI/Data/Christian_Arch/src/cnn_similarity_analysis/experiments"
     knn_path = "/localhome/prathmeshmadhu/work/EFI/Data/Christian_Arch/src/cnn_similarity_analysis/knn"
     pretrained_path = "/localhome/prathmeshmadhu/work/EFI/Data/Christian_Arch/src/cnn_similarity_analysis/resources"
-else:
+elif osuname == "lme242":
     data_path = "/cluster/open_datasets/Omniart"
     database_path = "/cluster/yinan/cnn_similarity_analysis/databases"
     visualization_path = "/cluster/yinan/cnn_similarity_analysis/visualizations"
