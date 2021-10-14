e35648dd3f8706bad40c625fcb29c01317b1cc5e
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-10-14 19:16:13 +0800

    Update model.py

--- src/lib/siamese/model.py
+++ src/lib/siamese/model.py
@@ -187,12 +187,13 @@ class TripletSiameseNetwork(nn.Module):
         # )
         #
         self.fc2 = nn.Sequential(
+            nn.ReLU(inplace=True),
             nn.Linear(1000, 512),
             # nn.Dropout2d(p=0.5),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, 128)
+            # nn.ReLU(inplace=True),
+            # nn.Linear(512, 256),
+            # nn.ReLU(inplace=True),
+            # nn.Linear(256, 128)
         )
 
         self.score = nn.PairwiseDistance(p=2)
