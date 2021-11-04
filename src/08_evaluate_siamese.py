ead060ccd8dfbda50c26992cda09415dee24cb23
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-04 22:54:47 +0800

    Update 08_evaluate_siamese.py

--- src/08_evaluate_siamese.py
+++ src/08_evaluate_siamese.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('/cluster/yinan/cnn_similarity_analysis/')
 from lib.io import *
-from lib.metrics import generate_5_matched_names, confusion_matrix
+from lib.metrics import generate_5_matched_names, confusion_matrix, calculate_top_accuracy
 from src.lib.siamese.args import siamese_args
 
 
@@ -23,33 +23,42 @@ def evaluation(args):
     miss_5 = 0
     precision = []
 
-    for i in range(len(q_names)):
-        vec = q_vectors[i]
-        # diff = db_vectors - vec
-        # l2_distance = np.linalg.norm(diff, axis=1)
-        # matched_index = np.argsort(l2_distance)[:5]
-        matched_names = generate_5_matched_names(vec, db_vectors, db_names)
-        matched_list.append(matched_names)
-        if matched_names[0] == db_names[i]:
-            hit += 1
-        else:
-            miss += 1
-        if db_names[i] in matched_names:
-            hit_5 += 1
-        else:
-            miss_5 += 1
-        tp, tn, fp, fn = confusion_matrix(vec, db_vectors, i, args.threshold)
-        precision.append(tp/(tp+fp))
-    mAP = np.mean(np.asarray(precision))
-    accuracy = hit / (hit + miss)
-    accuracy_5 = hit_5 / (hit_5 + miss_5)
-    print('TOP_1 accuracy:{}'.format(accuracy))
-    print('TOP_5 accuracy:{}'.format(accuracy_5))
-    print('mAP: {}'.format(mAP))
+    # for i in range(len(q_names)):
+    #     vec = q_vectors[i]
+    #     # diff = db_vectors - vec
+    #     # l2_distance = np.linalg.norm(diff, axis=1)
+    #     # matched_index = np.argsort(l2_distance)[:5]
+    #     matched_names = generate_5_matched_names(vec, db_vectors, db_names)
+    #     matched_list.append(matched_names)
+    #     if matched_names[0] == db_names[i]:
+    #         hit += 1
+    #     else:
+    #         miss += 1
+    #     if db_names[i] in matched_names:
+    #         hit_5 += 1
+    #     else:
+    #         miss_5 += 1
+    #     tp, tn, fp, fn = confusion_matrix(vec, db_vectors, i, args.threshold)
+    #     precision.append(tp/(tp+fp))
+    # mAP = np.mean(np.asarray(precision))
+    # accuracy = hit / (hit + miss)
+    # accuracy_5 = hit_5 / (hit_5 + miss_5)
 
-    fw = open(args.matched_f, 'wb')
-    pickle.dump(matched_list, fw)
-    fw.close()
+    accuracy_p1p2, accuracy_5_p1p2 = calculate_top_accuracy(gt_p1p2, p1_vectors, p2_vectors)
+    accuracy_p2p3, accuracy_5_p2p3 = calculate_top_accuracy(gt_p2p3, p2_vectors, p3_vectors)
+    accuracy_p1p3, accuracy_5_p1p3 = calculate_top_accuracy(gt_p1p3, p1_vectors, p3_vectors)
+
+    # print('mAP: {}'.format(mAP))
+    print('TOP_1 accuracy between P1 and P2:{}'.format(accuracy_p1p2))
+    print('TOP_1 accuracy between P2 and P3:{}'.format(accuracy_p2p3))
+    print('TOP_1 accuracy between P1 and P3:{}'.format(accuracy_p1p3))
+    print('TOP_5 accuracy between P1 and P2:{}'.format(accuracy_5_p1p2))
+    print('TOP_5 accuracy between P2 and P3:{}'.format(accuracy_5_p2p3))
+    print('TOP_5 accuracy between P1 and P3:{}'.format(accuracy_5_p1p3))
+
+    # fw = open(args.matched_f, 'wb')
+    # pickle.dump(matched_list, fw)
+    # fw.close()
 
     # if visualization:
     #     test_list = generate_validation_dataset(query_images, groundtruth_list, train_images, 50)
