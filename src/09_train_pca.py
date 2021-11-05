b747bf2889c6f654e366c80afa8ed4d0c1e4cff7
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-05 22:54:50 +0800

    Update 09_train_pca.py

--- src/09_train_pca.py
+++ src/09_train_pca.py
@@ -1,22 +1,38 @@
 import numpy as np
 import torch
+import time
 from torch.utils.data import DataLoader
 from src.lib.siamese.model import load_siamese_checkpoint
+from src.data.siamese_dataloader import ImageList
+from src.lib.siamese.dataset import get_transforms
+from sklearn.decomposition import PCA
+import joblib
 
 
-def gem_npy(x, p=3, eps=1e-6):
-    x = np.clip(x, a_min=eps, a_max=np.inf)
-    x = x ** p
-    x = x.mean(axis=0)
-    return x ** (1. / p)
+def generate_features(args, net, image_names, data_loader):
+    features_list = list()
+    images_list = list()
+    t0 = time.time()
+    with torch.no_grad():
+        for no, data in enumerate(data_loader):
+            images = data
+            images = images.to(args.device)
+            feats = net.forward_once(images)
+            features_list.append(feats.cpu().numpy())
+            images_list.append(images.cpu().numpy())
+    t1 = time.time()
+    features = np.vstack(features_list)
+    print(f"image_description_time: {(t1 - t0) / len(image_names):.5f} s per image")
+    return features
+
+
+def generate_pca_features():
+    pass
 
 
 def train(args):
     if args.device == "gpu":
         print("hardware_image_description:", torch.cuda.get_device_name(0))
-
-    net = load_siamese_checkpoint(args.model, args.checkpoint)
-    net.
 
     if args.dataset == "image_collation":
         d1_images = [args.d1 + 'illustration/' + l.strip() for l in open(args.d1 + 'files.txt', "r")]
@@ -24,6 +40,29 @@ def train(args):
         d3_images = [args.d3 + 'illustration/' + l.strip() for l in open(args.d3 + 'files.txt', "r")]
 
         train_images = d1_images + d2_images + d3_images
+
+    transforms = get_transforms(args)
+    train_dataset = ImageList(dataset=train_images, transform=transforms)
+    train_loader = DataLoader(dataset=train_dataset, shuffle=False, num_workers=args.num_workers,
+                              batch_size=args.batch_size)
+
+    net = load_siamese_checkpoint(args.model, args.checkpoint)
+    net.to(args.device)
+
+    train_features = generate_features(args, net, train_images, train_loader)
+
+    estimator = PCA(n_components=256)
+    estimator.fit(train_features)
+    joblib.dump(estimator, args.pca)
+
+
+
+
+
+
+
+
+
 
 
 
