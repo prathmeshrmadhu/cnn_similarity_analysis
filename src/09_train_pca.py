90e1c6104357e1e48bfee0bcc63d23287d846e0a
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-05 22:17:15 +0800

    Update 09_train_pca.py

--- src/09_train_pca.py
+++ src/09_train_pca.py
@@ -1,8 +1,7 @@
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-from lib.io import read_config
-from src.lib.siamese.dataset import generate_train_dataset, get_transforms, add_file_list
+from src.lib.siamese.model import load_siamese_checkpoint
 
 
 def gem_npy(x, p=3, eps=1e-6):
@@ -16,12 +15,17 @@ def train(args):
     if args.device == "gpu":
         print("hardware_image_description:", torch.cuda.get_device_name(0))
 
+    net = load_siamese_checkpoint(args.model, args.checkpoint)
+    net.
+
     if args.dataset == "image_collation":
         d1_images = [args.d1 + 'illustration/' + l.strip() for l in open(args.d1 + 'files.txt', "r")]
         d2_images = [args.d2 + 'illustration/' + l.strip() for l in open(args.d2 + 'files.txt', "r")]
         d3_images = [args.d3 + 'illustration/' + l.strip() for l in open(args.d3 + 'files.txt', "r")]
 
         train_images = d1_images + d2_images + d3_images
+
+
 
 
 
