e32112515f9fe2cb9e539a359d24dedc4fc768d7
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-05 23:02:50 +0800

    Update 09_train_pca.py

--- src/09_train_pca.py
+++ src/09_train_pca.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import time
 from torch.utils.data import DataLoader
+from src.lib.siamese.args import siamese_args
 from src.lib.siamese.model import load_siamese_checkpoint
 from src.data.siamese_dataloader import ImageList
 from src.lib.siamese.dataset import get_transforms
@@ -26,8 +27,10 @@ def generate_features(args, net, image_names, data_loader):
     return features
 
 
-def generate_pca_features():
-    pass
+# def generate_pca_features(features, estimator, image_names, save_path):
+#     pca_features = estimator.transform(features)
+#     write_pickle_descriptors(features, image_names, save_path)
+#     print(f"writing descriptors to {save_path}")
 
 
 def train(args):
@@ -48,12 +51,23 @@ def train(args):
 
     net = load_siamese_checkpoint(args.model, args.checkpoint)
     net.to(args.device)
+    net.eval()
 
     train_features = generate_features(args, net, train_images, train_loader)
 
     estimator = PCA(n_components=256)
     estimator.fit(train_features)
     joblib.dump(estimator, args.pca_file)
+
+
+
+if __name__ == "__main__":
+
+    pca_args = siamese_args()
+    if pca_args.device == "cuda:0":
+        print("hardware_image_description:", torch.cuda.get_device_name(0))
+
+    train(pca_args)
 
 
 
