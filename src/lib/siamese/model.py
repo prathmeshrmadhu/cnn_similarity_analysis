adbb3167416a2d1a6e18c3235c23928a55363b9a
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-08 18:55:00 +0800

    Update model.py

--- src/lib/siamese/model.py
+++ src/lib/siamese/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+import numpy as np
 from collections import OrderedDict, defaultdict
 
 from efficientnet_pytorch import EfficientNet
@@ -199,7 +200,7 @@ class TripletSiameseNetwork(nn.Module):
         self.score = nn.PairwiseDistance(p=2)
 
     def gem(self, x, p=3, eps=1e-6):
-        x = torch.clamp(x, eps, torch.inf)
+        x = torch.clamp(x, eps, np.inf)
         x = x ** p
         x = F.adaptive_avg_pool2d(x, (1, 1))
         return x ** (1. / p)
