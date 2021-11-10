217449ae03e5be1d053a830728b2d8b9c79b6cf6
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-10 17:33:43 +0800

    Update 08_evaluate_siamese.py

--- src/08_evaluate_siamese.py
+++ src/08_evaluate_siamese.py
@@ -28,6 +28,15 @@ def evaluation(args):
     accuracy_cos = (hit_p1p2_cos + hit_p2p3_cos + hit_p1p3_cos) / (len(gt_p1p2) + len(gt_p1p2) + len(gt_p1p2))
     accuracy_5_cos = (hit_5_p1p2_cos + hit_5_p2p3_cos + hit_5_p1p3_cos) / (len(gt_p1p2) + len(gt_p1p2) + len(gt_p1p2))
 
+    tp_d1d2, tn_d1d2, fp_d1d2, fn_d1d2 = confusion_matrix(p1_vectors, p2_vectors, gt_p1p2, args.threshold_d)
+    tp_d2d3, tn_d2d3, fp_d2d3, fn_d2d3 = confusion_matrix(p2_vectors, p3_vectors, gt_p2p3, args.threshold_d)
+    tp_d1d3, tn_d1d3, fp_d1d3, fn_d1d3 = confusion_matrix(p1_vectors, p3_vectors, gt_p1p3, args.threshold_d)
+    mAP = (tp_d1d2 + tp_d2d3 + tp_d1d3) / (tp_d1d2 + tp_d2d3 + tp_d1d3 + fp_d1d2 + fp_d2d3 + fp_d1d3)
+
+    tp_d1d2, tn_d1d2, fp_d1d2, fn_d1d2 = confusion_matrix(p1_vectors, p2_vectors, gt_p1p2, args.threshold_s, False)
+    tp_d2d3, tn_d2d3, fp_d2d3, fn_d2d3 = confusion_matrix(p2_vectors, p3_vectors, gt_p2p3, args.threshold_s, False)
+    tp_d1d3, tn_d1d3, fp_d1d3, fn_d1d3 = confusion_matrix(p1_vectors, p3_vectors, gt_p1p3, args.threshold_s, False)
+    mAP_cos = (tp_d1d2 + tp_d2d3 + tp_d1d3) / (tp_d1d2 + tp_d2d3 + tp_d1d3 + fp_d1d2 + fp_d2d3 + fp_d1d3)
 
 
     # print('mAP: {}'.format(mAP))
@@ -36,6 +45,9 @@ def evaluation(args):
     print('\n')
     print('TOP_1 accuracy with cosine similarity:{}'.format(accuracy_cos))
     print('TOP_5 accuracy with cosine similarity:{}'.format(accuracy_5_cos))
+    print('\n')
+    print('mAP: {}'.format(mAP))
+    print('mAP with cosine similarity: {}'.format(mAP_cos))
 
 
     # fw = open(args.matched_f, 'wb')
