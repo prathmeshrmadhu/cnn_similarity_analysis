a6630aeb3ce7ad1a00fb7bd20abe564e054b0881
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-13 20:51:50 +0800

    Update 09_embedding_pca_features.py

--- src/09_embedding_pca_features.py
+++ src/09_embedding_pca_features.py
@@ -64,10 +64,17 @@ def embedding_features(args):
         p1_images = [args.p1 + 'illustration/' + l.strip() for l in open(args.p1 + 'files.txt', "r")]
         p2_images = [args.p2 + 'illustration/' + l.strip() for l in open(args.p2 + 'files.txt', "r")]
         p3_images = [args.p3 + 'illustration/' + l.strip() for l in open(args.p3 + 'files.txt', "r")]
+        d1_images = [args.d1 + 'illustration/' + l.strip() for l in open(args.d1 + 'files.txt', "r")]
+        d2_images = [args.d2 + 'illustration/' + l.strip() for l in open(args.d2 + 'files.txt', "r")]
+        d3_images = [args.d3 + 'illustration/' + l.strip() for l in open(args.d3 + 'files.txt', "r")]
+
 
         p1_dataset = ImageList(p1_images, transform=transforms)
         p2_dataset = ImageList(p2_images, transform=transforms)
         p3_dataset = ImageList(p3_images, transform=transforms)
+        d1_dataset = ImageList(d1_images, transform=transforms)
+        d2_dataset = ImageList(d2_images, transform=transforms)
+        d3_dataset = ImageList(d3_images, transform=transforms)
 
         p1_loader = DataLoader(dataset=p1_dataset, shuffle=False, num_workers=args.num_workers,
                                batch_size=args.batch_size)
@@ -75,14 +82,26 @@ def embedding_features(args):
                                batch_size=args.batch_size)
         p3_loader = DataLoader(dataset=p3_dataset, shuffle=False, num_workers=args.num_workers,
                                batch_size=args.batch_size)
+        d1_loader = DataLoader(dataset=d1_dataset, shuffle=False, num_workers=args.num_workers,
+                               batch_size=args.batch_size)
+        d2_loader = DataLoader(dataset=d2_dataset, shuffle=False, num_workers=args.num_workers,
+                               batch_size=args.batch_size)
+        d3_loader = DataLoader(dataset=d3_dataset, shuffle=False, num_workers=args.num_workers,
+                               batch_size=args.batch_size)
 
         p1_features = generate_features(args, net, p1_images, p1_loader)
         p2_features = generate_features(args, net, p2_images, p2_loader)
         p3_features = generate_features(args, net, p3_images, p3_loader)
+        d1_features = generate_features(args, net, p1_images, p1_loader)
+        d2_features = generate_features(args, net, p2_images, p2_loader)
+        d3_features = generate_features(args, net, p3_images, p3_loader)
 
         generate_pca_features(p1_features, p1_images, args.p1_f)
         generate_pca_features(p2_features, p2_images, args.p2_f)
         generate_pca_features(p3_features, p3_images, args.p3_f)
+        generate_pca_features(d1_features, d1_images, args.d1_f)
+        generate_pca_features(d2_features, d2_images, args.d2_f)
+        generate_pca_features(d3_features, d3_images, args.d3_f)
 
 
 if __name__ == "__main__":
