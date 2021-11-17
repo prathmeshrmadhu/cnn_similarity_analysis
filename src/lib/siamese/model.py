3b9ae8335d0d401250e4fd6a8b502ce68ec30b1b
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-11-17 17:18:01 +0800

    Update model.py

--- src/lib/siamese/model.py
+++ src/lib/siamese/model.py
@@ -234,7 +234,7 @@ class TripletSiameseNetwork(nn.Module):
             x = self.head.layer1(x)
             x = self.head.layer2(x)
             x = self.head.layer3(x)
-            x = self.head.layer4(x)
+            # x = self.head.layer4(x)
             # x = self.gem(x)
             # x = self.flatten(x)
             # x = self.fc1(x)
