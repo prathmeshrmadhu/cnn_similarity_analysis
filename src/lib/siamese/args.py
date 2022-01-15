f200e351894e0cc05ba8b1c1e2f0d3b06f86ef65
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-01-15 22:55:13 +0800

    Update args.py

--- src/lib/siamese/args.py
+++ src/lib/siamese/args.py
@@ -74,7 +74,7 @@ def siamese_args():
     aa('--matched_f', default=None, help="save matched result to this folder")
     aa('--test_f', default=None, help="save test result to this folder")
     aa('--net', default=EXP_PATH + 'models/', help="save network parameters to this folder")
-    aa('--plots', default="isc2021/data/images/siamese/", help="save visualized test result to this folder")
+    aa('--plots', default=EXP_PATH + 'plots/', help="save visualized test result to this folder")
     aa('--p1_f', default=None, help="write p1 features to this file")
     aa('--p2_f', default=None, help="write p2 features to this file")
     aa('--p3_f', default=None, help="write p3 features to this file")
