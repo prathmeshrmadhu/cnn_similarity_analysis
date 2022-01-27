abe447bac4c568c9a97cb56593534baa3cd1fffb
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-01-27 17:07:35 +0800

    Update model.py

--- src/lib/siamese/model.py
+++ src/lib/siamese/model.py
@@ -307,6 +307,7 @@ class TripletSiameseNetwork(nn.Module):
 class TripletSiameseNetwork_custom(nn.Module):
     def __init__(self, model, checkpoint='/cluster/yinan/isc2021/data/multigrain_joint_3B_0.5.pth'):
         super(TripletSiameseNetwork_custom, self).__init__()
+        self.model = model
         self.head = load_siamese_checkpoint(model, checkpoint)
         self.flatten = nn.Flatten()
         self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
@@ -318,23 +319,42 @@ class TripletSiameseNetwork_custom(nn.Module):
         return x ** (1. / p)
 
     def forward_once(self, x):
-        x = self.head.conv1(x)
-        x = self.head.bn1(x)
-        x = self.head.relu(x)
-        x = self.head.maxpool(x)
+        if self.model == 'resnet50':
+            x = self.head.conv1(x)
+            x = self.head.bn1(x)
+            x = self.head.relu(x)
+            x = self.head.maxpool(x)
 
-        x1 = self.head.layer1(x)
-        x2 = self.head.layer2(x1)
-        x3 = self.head.layer3(x2)
-        x4 = self.head.layer4(x3)
-        x1 = self.gem(x1)
-        x1 = self.flatten(x1)
-        x2 = self.gem(x2)
-        x2 = self.flatten(x2)
-        x3 = self.gem(x3)
-        x3 = self.flatten(x3)
-        x4 = self.gem(x4)
-        x4 = self.flatten(x4)
+            x1 = self.head.layer1(x)
+            x2 = self.head.layer2(x1)
+            x3 = self.head.layer3(x2)
+            x4 = self.head.layer4(x3)
+            x1 = self.gem(x1)
+            x1 = self.flatten(x1)
+            x2 = self.gem(x2)
+            x2 = self.flatten(x2)
+            x3 = self.gem(x3)
+            x3 = self.flatten(x3)
+            x4 = self.gem(x4)
+            x4 = self.flatten(x4)
+
+        elif self.model == 'vgg':
+            '''relu1_2'''
+            x1 = self.head[:3](x)
+            '''relu2_2'''
+            x2 = self.head[4:9](x1)
+            '''relu3_3'''
+            x4 = self.head[9:16](x2)
+            '''relu4_3'''
+            x3 = self.head[16:23](4)
+            x1 = F.adaptive_max_pool2d(x1, (1, 1))
+            x1 = self.flatten(x1)
+            x2 = F.adaptive_max_pool2d(x2, (1, 1))
+            x2 = self.flatten(x2)
+            x3 = F.adaptive_max_pool2d(x3, (1, 1))
+            x3 = self.flatten(x3)
+            x4 = F.adaptive_max_pool2d(x4, (1, 1))
+            x4 = self.flatten(x4)
         return x1, x2, x3, x4
 
     def forward(self, input1, input2, input3):
