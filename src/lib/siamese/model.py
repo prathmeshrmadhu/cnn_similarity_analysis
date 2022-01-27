7cff3b60a194a37de8b744132b544b6ee4effc3b
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-01-27 17:37:15 +0800

    Update model.py

--- src/lib/siamese/model.py
+++ src/lib/siamese/model.py
@@ -346,7 +346,7 @@ class TripletSiameseNetwork_custom(nn.Module):
             '''relu3_3'''
             x4 = self.head[9:16](x2)
             '''relu4_3'''
-            x3 = self.head[16:23](4)
+            x3 = self.head[16:23](x4)
 
             x1 = F.adaptive_max_pool2d(x1, (1, 1))
             x1 = self.flatten(x1)
