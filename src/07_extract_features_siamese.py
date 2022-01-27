e2bf7edcf98d00ab986203d89088d93cb0a40a4f
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-01-27 16:30:03 +0800

    Update 07_extract_features_siamese.py

--- src/07_extract_features_siamese.py
+++ src/07_extract_features_siamese.py
@@ -65,10 +65,14 @@ def extract_features(args):
         net = TripletSiameseNetwork_custom(args.model)
     elif args.loss == "normal":
         net = TripletSiameseNetwork(args.model, args.method)
-    if args.net:
-        print('load trained model:{}'.format(args.net + args.checkpoint))
+
+    try:
         state_dict = torch.load(args.net + args.checkpoint)
+        print('load trained model:{}'.format(args.net + args.checkpoint))
         net.load_state_dict(state_dict)
+    except:
+        pass
+
     net.eval()
     net.to(args.device)
     print("checkpoint {} loaded\n".format(args.checkpoint))
