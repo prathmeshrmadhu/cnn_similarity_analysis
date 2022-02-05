54c3b8c3b83f9750d9b2593beb1638cf858c56ba
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-02-05 22:11:27 +0800

    Update 10_kevinmusgrave_library.py

--- src/10_kevinmusgrave_library.py
+++ src/10_kevinmusgrave_library.py
@@ -6,10 +6,9 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-from src.lib.io import read_config, generate_train_list, generate_val_list
 import sys
 sys.path.append('/cluster/yinan/yinan_cnn/cnn_similarity_analysis/')
-
+from src.lib.io import read_config, generate_train_list, generate_val_list
 from src.lib.loss import TripletLoss, CustomLoss, CustomLoss_vgg
 from src.lib.siamese.args import siamese_args
 from src.lib.siamese.dataset import generate_train_dataset, get_transforms, add_file_list
