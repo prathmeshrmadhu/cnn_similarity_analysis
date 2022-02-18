0c841634ddca0c3a79a0dd982094bf1546c18a04
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-02-18 21:29:10 +0800

    Update args.py

--- src/lib/siamese/args.py
+++ src/lib/siamese/args.py
@@ -85,7 +85,7 @@ def siamese_args():
     group = parser.add_argument_group('optuna parameters')
     aa('--upper_bound', type=float, default=None, help="upper bound of the hyperparameter")
     aa('--lower_bound', type=float, default=None, help="lower bound of the hyperparameter")
-    aa('--num_expriment', default=20, type=int, help="number of search expriments")
+    aa('--num_experiment', default=20, type=int, help="number of search experiments")
 
     args = parser.parse_args()
     args.scales = [float(x) for x in args.scales.split(",")]
