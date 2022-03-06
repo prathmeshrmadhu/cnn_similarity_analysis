5545f0f33873c1d57e5a02a66fb2f292c5ab6b24
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-03-06 21:55:45 +0800

    Update 06_train_triplet_siamese.py

--- src/06_train_triplet_siamese.py
+++ src/06_train_triplet_siamese.py
@@ -111,7 +111,7 @@ def train(args, augmentations_list):
         train_file = args.data_path + args.train_list
         train_frame_o = pd.read_csv(train_file)
         if args.mining_mode == 'offline':
-            train_frame = train_frame_o[:20000]
+            train_frame = train_frame_o[:args.len]
         val_file = args.data_path + args.val_list
         val_frame = pd.read_csv(val_file)
 
@@ -263,13 +263,13 @@ def train(args, augmentations_list):
                     break
 
             if args.train_dataset == "the_MET":
-                train_frame = train_frame_o.sample(n=20000)
+                train_frame = train_frame_o.sample(n=args.len)
                 train_frame = train_frame.reset_index()
 
         if args.train_dataset == 'the_MET':
             image_pairs = TripletTrainList(args.data_path, train_frame, transform=transforms, imsize=args.imsize,
                                            argumentation=augmentations_list, mode=args.mining_mode)
-            num_triplets = 10000
+            num_triplets = args.len
         else:
             image_pairs = TripletValList(train_list, transform=transforms, imsize=args.imsize, argumentation=augmentations_list)
         train_dataloader = DataLoader(dataset=image_pairs, shuffle=True, num_workers=args.num_workers,
