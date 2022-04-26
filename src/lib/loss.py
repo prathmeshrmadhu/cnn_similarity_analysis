9e126b34238d593792ded2fd468ec8fbfad51089
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-04-26 15:25:50 +0800

    Update loss.py

--- src/lib/loss.py
+++ src/lib/loss.py
@@ -125,16 +125,13 @@ class SimCLR_Loss(torch.nn.Module):
             print(neg_sum)
         return loss2
 
+
 class FocalLoss(torch.nn.Module):
     def __int__(self):
         super(TripletLoss, self).__init__()
 
     def forward(self, p, label, alpha, gamma):
-        if label == 1:
-            pt = p
-        elif label == 0:
-            pt == 1 - p
-        else:
-            print('label must be 0 or 1')
-        loss = -alpha * (1 - pt)^gamma * torch.log(pt)
+        pt = p
+        pt[label == 0] = 1 - pt[label == 0]
+        loss = torch.mean(-alpha * (1 - pt)^gamma * torch.log(pt))
         return loss
